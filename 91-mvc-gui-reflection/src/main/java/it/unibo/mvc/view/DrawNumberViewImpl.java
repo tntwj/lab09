6d0e7da0ddec9762c7612ae8f33181b4fd53371b
Weijie Fu <weijie.fu@studio.unibo.it>
2023-11-21 16:15:41 +0100

    create class that impl DrawNumberView, methods TODO

---
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberViewImpl.java
@@ -0,0 +1,28 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberViewImpl implements DrawNumberView {
+
+    @Override
+    public void setController(DrawNumberController observer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setController'");
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'result'");
+    }
+
+    
+}
