2cc989e4cf9988f66e3aa220b42d1b6ec8f6baec
Weijie Fu <weijie.fu@studio.unibo.it>
2023-11-21 15:54:06 +0100

    impl multiple views as arrayList

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -4,6 +4,8 @@ import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -13,7 +15,7 @@ import java.util.Objects;
 public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
-    private DrawNumberView view;
+    private List<DrawNumberView> views = new ArrayList<>();
 
     /**
      * Builds a new game controller provided a game model.
@@ -27,17 +29,14 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
     @Override
     public void addView(final DrawNumberView view) {
         Objects.requireNonNull(view, "Cannot set a null view");
-        if (this.view != null) {
-            throw new IllegalStateException("The view is already set! Multiple views are not supported");
-        }
-        this.view = view;
+        this.views.add(view);
         view.setController(this);
         view.start();
     }
 
     @Override
     public void newAttempt(final int n) {
-        Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));
+        //Objects.requireNonNull(views, "There is no view attached!").result(model.attempt(n));
     }
 
     @Override
